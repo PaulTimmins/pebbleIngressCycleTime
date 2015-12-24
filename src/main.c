03c8d53b14b738f0e6bd702a3ba6919b4ac9a49b
PaulTimmins <paul@timmins.net>
2015-12-23 19:39:10 -0500

    It works!
    Lots of changes, hooboy

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <pebble.h>
 
-Window *my_window;
+static Window *my_window;
 TextLayer *tl_cycle;
 TextLayer *tl_cp;
 TextLayer *tl_countdown;
@@ -16,6 +16,8 @@ static char region[] = "-REGION-";
 static uint32_t mydata = 0; //you want ants? That's how you get ants.
 static uint32_t lastcheck = 0; //last time we asked the phone for region/tz data
 
+static uint32_t updatingtime = 0;
+
 #define START_TIME_SEC 0  //checkpoints actually can be done without special modulus - unix epoch is okay
 #define CYCLE_SEC 630000
 #define CP_SEC 18000
@@ -23,8 +25,14 @@ static uint32_t lastcheck = 0; //last time we asked the phone for region/tz data
 #define SHOW_CP_NUM 3 // number of checkpoints displayed
 #define CP_DATA_SIZE 6 // number of characters per checkpoint
 
-char buffer[6][BUF_SIZE];
+static char buffer[7][BUF_SIZE];
+//char battstate[BUF_SIZE];
+static char tickstate;
 
+static void handle_tick(struct tm *tick_time, TimeUnits units_changed);
+static void appmessage_init(void);
+static void main_window_load(Window *window);
+static void main_window_unload(Window *window);
 
 char *translate_error(AppMessageResult result) {
   switch (result) {
@@ -46,7 +54,7 @@ char *translate_error(AppMessageResult result) {
   }
 }
 
-void handle_conn(bool connected) {
+static void handle_conn(bool connected) {
     //APP_LOG( APP_LOG_LEVEL_ERROR , "connected toggle");
 	if (connected) {
     #ifdef PBL_COLOR
@@ -72,38 +80,57 @@ void handle_conn(bool connected) {
 	}
 }
 
-void handle_batt(BatteryChargeState charge) {
-  static char battstate[BUF_SIZE];
-  
+static void handle_batt(BatteryChargeState charge) {
+   if (updatingtime) {
+    APP_LOG( APP_LOG_LEVEL_ERROR , "called handle_batt UPDATING TIME ALREADY aieee");
+    return;
+  };
+        APP_LOG (APP_LOG_LEVEL_ERROR , "battery event old buffer (%s)",buffer[6]);
+   #ifdef PBL_COLOR
+
           //text_layer_set_background_color(tl_batt_layer, GColorBlack);
+          APP_LOG( APP_LOG_LEVEL_ERROR , "white");
+   
           text_layer_set_text_color(tl_batt_layer, GColorWhite);
-  #ifdef PBL_COLOR
+          APP_LOG( APP_LOG_LEVEL_ERROR , "set to white");
             if (charge.charge_percent <= 20)  {
+                APP_LOG( APP_LOG_LEVEL_ERROR , "red");
                 text_layer_set_text_color(tl_batt_layer, GColorRed);
             }
-            if (charge.charge_percent <= 50 && charge.charge_percent > 20)  {
+            if ((charge.charge_percent <= 50) && (charge.charge_percent > 20))  {
+                APP_LOG( APP_LOG_LEVEL_ERROR , "pastelyellow");
                 text_layer_set_text_color(tl_batt_layer, GColorPastelYellow);
             }
             if (charge.is_charging)  {
+                APP_LOG( APP_LOG_LEVEL_ERROR , "normalyellow");
                 text_layer_set_text_color(tl_batt_layer, GColorYellow);
             }
-            if (charge.charge_percent == 100) {
+            if (charge.charge_percent >= 90) {
+                APP_LOG( APP_LOG_LEVEL_ERROR , "green");
                 text_layer_set_text_color(tl_batt_layer, GColorGreen);
             }
   #endif
-  snprintf(battstate, BUF_SIZE, "%s: %d%%", charge.is_charging?"CHG":"BATT",charge.charge_percent);
-  text_layer_set_text(tl_batt_layer, battstate);
-  //APP_LOG( APP_LOG_LEVEL_ERROR , "handle batt");
+  
+  APP_LOG( APP_LOG_LEVEL_ERROR , "before snprintf %s: %d%%",charge.is_charging?"CHG":"BATT",charge.charge_percent);
+  snprintf(buffer[6], sizeof(buffer[6]), "%s: %d%%", charge.is_charging?"CHG":"BATT",charge.charge_percent);
+  APP_LOG( APP_LOG_LEVEL_ERROR , "after snprintf (%s) handle batt", buffer[6]);
+  //text_layer_set_text(tl_batt_layer, buffer[6]);
+  APP_LOG( APP_LOG_LEVEL_ERROR , "after textlayer (%s) handle batt", buffer[6]);
 }
 
-static void handle_tick(struct tm *tick_time, TimeUnits units_changed);
 
-void appmessage_init(void);
 
 static void update_time(bool fullupdate) {
+  if (updatingtime) {
+    APP_LOG( APP_LOG_LEVEL_ERROR , "called update_time UPDATING TIME ALREADY aieee");
+    return;
+  };
+  updatingtime=1;
+  APP_LOG(APP_LOG_LEVEL_ERROR , "updating time");
+    
   int32_t utcoffset;
   utcoffset =  mydata - 2400;
-   //APP_LOG( APP_LOG_LEVEL_ERROR , "%ld: utcoffset tick",utcoffset*60);
+   APP_LOG( APP_LOG_LEVEL_ERROR , "%ld: utcoffset tick",utcoffset*60);
 	time_t rt = time(NULL);
   uint32_t t = rt;
   uint32_t countdown;
@@ -123,6 +150,7 @@ static void update_time(bool fullupdate) {
 	}
 	
 	if(fullupdate){
+
       uint32_t next;
       uint32_t last;
       if (clock_is_timezone_set()) { //thanks, pebble. #sigh
@@ -141,9 +169,9 @@ static void update_time(bool fullupdate) {
         cycle = (t - offset) / CYCLE_SEC;
         cp = (t % CYCLE_SEC) / CP_SEC + 1;
       
-      //APP_LOG( APP_LOG_LEVEL_ERROR , "full update %ld: utcoffset, %lu: offset, %lu: cycle, %lu: checkpoint, %lu: year", utcoffset, offset, cycle, cp, year);
+      APP_LOG( APP_LOG_LEVEL_ERROR , "full update %ld: utcoffset, %lu: offset, %lu: cycle, %lu: checkpoint, %lu: year", utcoffset, offset, cycle, cp, year);
 
-		  snprintf(buffer[0], BUF_SIZE, "%lu.%02lu", year, cycle); //fuck you cloudpebble, these snprintfs are legit
+		  snprintf(buffer[0], BUF_SIZE, "%lu.%02lu", year, cycle); 
 		  snprintf(buffer[1], BUF_SIZE, "%02lu/35", cp);
 		  text_layer_set_text(tl_cycle, buffer[0]);
 		  text_layer_set_text(tl_cp, buffer[1]);
@@ -165,18 +193,29 @@ static void update_time(bool fullupdate) {
 	tms = gmtime((time_t*)&countdown);
   if (countdown <= 300) {
     strftime(buffer[2], BUF_SIZE, "%H:%M:%S", tms);
-    tick_timer_service_unsubscribe();
+    //tick_timer_service_unsubscribe();
+    if (tickstate == MINUTE_UNIT) {
     tick_timer_service_subscribe(SECOND_UNIT, &handle_tick);
+    APP_LOG( APP_LOG_LEVEL_ERROR , "ticks set to seconds");
+    tickstate = SECOND_UNIT;
+    };
   } else {
-    tick_timer_service_unsubscribe();
+    //tick_timer_service_unsubscribe();
+    if (tickstate == SECOND_UNIT) {
     tick_timer_service_subscribe(MINUTE_UNIT, &handle_tick);
+    APP_LOG( APP_LOG_LEVEL_ERROR , "ticks set to minutes");
+    tickstate = MINUTE_UNIT;
+    };
 	  strftime(buffer[2], BUF_SIZE, "%H:%M", tms);
   };
     text_layer_set_text(tl_countdown, buffer[2]);
   acttime = localtime((time_t*) &rt);
   strftime(buffer[5], BUF_SIZE, "%m/%d %H:%M", acttime);
   text_layer_set_text(tl_realtime, buffer[5]);
-
+  APP_LOG(APP_LOG_LEVEL_ERROR , "updating time complete");
+  //text_layer_set_text(tl_batt_layer, buffer[6]);
+  APP_LOG(APP_LOG_LEVEL_ERROR , "writing battery buffer");
+  updatingtime=0;
 }
 
 static void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
@@ -193,11 +232,11 @@ static void in_received_handler(DictionaryIterator *iter, void *context) {
   if (ofs->value->uint32) {
     if (ofs->value->uint32 != mydata) {
         persist_write_int(1,ofs->value->uint32); //store this for later otherwise don't
-        //APP_LOG (APP_LOG_LEVEL_ERROR , "%lu: utcoffset stored in slot 1", ofs->value->uint32);
+        APP_LOG (APP_LOG_LEVEL_ERROR , "%lu: utcoffset stored in slot 1", ofs->value->uint32);
     }
 	memcpy(&mydata,&ofs->value->uint32,sizeof(uint32_t));
 	strncpy(region, rgn->value->cstring, rgn->length);
-  //APP_LOG( APP_LOG_LEVEL_ERROR , "%lu: utcoffset received, %s: region",mydata,region);
+  APP_LOG( APP_LOG_LEVEL_ERROR , "%lu: utcoffset received, %s: region",mydata,region);
   text_layer_set_text(tl_region_layer, region);
   lastcheck = (uint32_t) time(NULL);
   update_time(true);
@@ -209,7 +248,7 @@ static void in_dropped_handler(AppMessageResult reason, void *context) {
    APP_LOG(APP_LOG_LEVEL_DEBUG, "In dropped: %i - %s", reason, translate_error(reason));
 }
 
-void appmessage_init(void) {
+static void appmessage_init(void) {
 	app_message_register_inbox_received(in_received_handler);
   app_message_register_inbox_dropped(in_dropped_handler);
  
@@ -218,25 +257,36 @@ void appmessage_init(void) {
   app_message_outbox_send();
 }
 
-void handle_init(void) {
+static void handle_init(void) {
 	my_window = window_create();
+  window_set_window_handlers(my_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
 	window_stack_push(my_window, true);
-	
-	GFont font_s = fonts_get_system_font(FONT_KEY_GOTHIC_18);
-	GFont font_m = fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD);
-  GFont font_24 = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
-	GFont font_b = fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD);
-	Layer *root_layer = window_get_root_layer(my_window);
+
+};
+
+static void main_window_unload(Window *window) {
+    //nothing
+};
+
+static void main_window_load(Window *window) {
+	//GFont font_s = fonts_get_system_font(FONT_KEY_GOTHIC_18);
+	//GFont font_m = fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD);
+  //GFont font_24 = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
+	//GFont font_b = fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD);
+	Layer *root_layer = window_get_root_layer(window);
 	GRect frame = layer_get_frame(root_layer);
 	
   #ifdef PBL_COLOR
-    window_set_background_color(my_window, GColorBlue);
+    window_set_background_color(window, GColorBlue);
   #else
-    window_set_background_color(my_window, GColorBlack);
+    window_set_background_color(window, GColorBlack);
   #endif
 	
 	tl_cycle = text_layer_create(GRect(0, 0, 70, 26));
-	text_layer_set_font(tl_cycle, font_24);
+	text_layer_set_font(tl_cycle, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
 	text_layer_set_text_alignment(tl_cycle, GTextAlignmentLeft);
   #ifdef PBL_COLOR
     text_layer_set_background_color(tl_cycle, GColorBlue);
@@ -247,7 +297,7 @@ void handle_init(void) {
 	#endif
 	
 	tl_cp = text_layer_create(GRect(70, 0, frame.size.w-70, 26));
-	text_layer_set_font(tl_cp, font_24);
+	text_layer_set_font(tl_cp, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
 	text_layer_set_text_alignment(tl_cp, GTextAlignmentRight);
 	#ifdef PBL_COLOR
     text_layer_set_background_color(tl_cp, GColorBlue);
@@ -259,7 +309,7 @@ void handle_init(void) {
 	
 
 	tl_countdown = text_layer_create(GRect(0, 25, frame.size.w, 30));
-	text_layer_set_font(tl_countdown, font_b);
+	text_layer_set_font(tl_countdown, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
 	text_layer_set_text_alignment(tl_countdown, GTextAlignmentCenter);
 	#ifdef PBL_COLOR
     text_layer_set_background_color(tl_countdown, GColorBlue);
@@ -270,7 +320,7 @@ void handle_init(void) {
 	#endif
 	
 	tl_list = text_layer_create(GRect(0, frame.size.h-25, frame.size.w, 20));
-	text_layer_set_font(tl_list, font_m);
+	text_layer_set_font(tl_list, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
 	text_layer_set_text_alignment(tl_list, GTextAlignmentCenter);
 	#ifdef PBL_COLOR
     text_layer_set_background_color(tl_list, GColorBlue);
@@ -281,10 +331,10 @@ void handle_init(void) {
 	#endif
 	
 	
-	img_res = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_RES); //48x67
-	bl_res = bitmap_layer_create(GRect(0, 60, 50, 70));
+  img_res = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_RES); //48x67
+  bl_res = bitmap_layer_create(GRect(0, 60, 50, 70));
   bitmap_layer_set_bitmap(bl_res, img_res);
-	bitmap_layer_set_alignment(bl_res, GAlignLeft); 
+  bitmap_layer_set_alignment(bl_res, GAlignLeft); 
 
 #ifdef PBL_COLOR
   bitmap_layer_set_background_color(bl_res, GColorBlue);
@@ -293,7 +343,7 @@ void handle_init(void) {
 #endif
     
   tl_realtime = text_layer_create(GRect(55, 60, frame.size.w-60, 20));
-	text_layer_set_font(tl_realtime, font_m);
+	text_layer_set_font(tl_realtime, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
 	text_layer_set_text_alignment(tl_realtime, GTextAlignmentRight);
 	#ifdef PBL_COLOR
     text_layer_set_background_color(tl_realtime, GColorBlue);
@@ -304,7 +354,7 @@ void handle_init(void) {
 	#endif
     
 	tl_conn_layer = text_layer_create(GRect(55, 80, frame.size.w-60, 20));
-  text_layer_set_font(tl_conn_layer, font_m);
+  text_layer_set_font(tl_conn_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
 	text_layer_set_text_alignment(tl_conn_layer, GTextAlignmentRight);
 	#ifdef PBL_COLOR
     text_layer_set_background_color(tl_conn_layer, GColorBlue);
@@ -316,7 +366,7 @@ void handle_init(void) {
 	text_layer_set_text(tl_conn_layer, "BT: --");
 	
   tl_batt_layer = text_layer_create(GRect(55, 100, frame.size.w-60, 20));
-  text_layer_set_font(tl_batt_layer, font_m);
+  text_layer_set_font(tl_batt_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
 	text_layer_set_text_alignment(tl_batt_layer, GTextAlignmentRight);
 	#ifdef PBL_COLOR
     text_layer_set_background_color(tl_batt_layer, GColorBlue);
@@ -325,10 +375,11 @@ void handle_init(void) {
     text_layer_set_background_color(tl_batt_layer, GColorBlack);
     text_layer_set_text_color(tl_batt_layer, GColorWhite);
 	#endif
-	text_layer_set_text(tl_batt_layer, "BATT: --");
+	snprintf(buffer[6],sizeof(buffer[6]),"BATT: --");
+  text_layer_set_text(tl_batt_layer, buffer[6]);
  	
 	tl_region_layer = text_layer_create(GRect(30, 120, frame.size.w-35, 20));
- 	text_layer_set_font(tl_region_layer, font_s);
+ 	text_layer_set_font(tl_region_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
 	text_layer_set_text_alignment(tl_region_layer, GTextAlignmentRight);
 	#ifdef PBL_COLOR
     text_layer_set_background_color(tl_region_layer, GColorBlue);
@@ -337,12 +388,12 @@ void handle_init(void) {
     text_layer_set_background_color(tl_region_layer, GColorBlack);
     text_layer_set_text_color(tl_region_layer, GColorWhite);
 	#endif
-	text_layer_set_text(tl_region_layer, "-REGION-");	
+	text_layer_set_text(tl_region_layer, "-REGION-");
  
 	layer_add_child(root_layer, bitmap_layer_get_layer(bl_res));
 	layer_add_child(root_layer, text_layer_get_layer(tl_cycle));	
-	layer_add_child(root_layer, text_layer_get_layer(tl_cp));	
-	layer_add_child(root_layer, text_layer_get_layer(tl_countdown));	
+	layer_add_child(root_layer, text_layer_get_layer(tl_cp));
+	layer_add_child(root_layer, text_layer_get_layer(tl_countdown));
 	layer_add_child(root_layer, text_layer_get_layer(tl_list));
 	layer_add_child(root_layer, text_layer_get_layer(tl_conn_layer));
 	layer_add_child(root_layer, text_layer_get_layer(tl_batt_layer));
@@ -350,20 +401,19 @@ void handle_init(void) {
 	layer_add_child(root_layer, text_layer_get_layer(tl_realtime));
   
 	//tick_timer_service_subscribe(SECOND_UNIT, &handle_tick);
-  tick_timer_service_subscribe(MINUTE_UNIT, &handle_tick);
 	bluetooth_connection_service_subscribe(handle_conn);
 	handle_conn(bluetooth_connection_service_peek());
 	battery_state_service_subscribe(handle_batt);
 	handle_batt(battery_state_service_peek());
   mydata=persist_read_int(1); // read in the last known offset
-  //APP_LOG (APP_LOG_LEVEL_ERROR , "%lu: utcoffset retrieved from slot 1", mydata);
-
-	update_time(true);
+  APP_LOG (APP_LOG_LEVEL_ERROR , "%lu: utcoffset retrieved from slot 1", mydata);
   appmessage_init();
+  tickstate=SECOND_UNIT;
+  tick_timer_service_subscribe(SECOND_UNIT, &handle_tick);
 }
 
 
-void handle_deinit(void) {
+static void handle_deinit(void) {
 	tick_timer_service_unsubscribe();
 	battery_state_service_unsubscribe();
 	bluetooth_connection_service_unsubscribe();
@@ -373,11 +423,10 @@ void handle_deinit(void) {
 	//text_layer_destroy(tl_list);
 	//text_layer_destroy(tl_conn_layer);
 	//text_layer_destroy(tl_batt_layer);
- 	//gbitmap_destroy(img_res);
-	//bitmap_layer_destroy(bl_res);
-	//window_destroy(my_window);
+ 	gbitmap_destroy(img_res);
+	bitmap_layer_destroy(bl_res);
+	window_destroy(my_window);
 }
-
 
 int main(void) {
 	handle_init();
